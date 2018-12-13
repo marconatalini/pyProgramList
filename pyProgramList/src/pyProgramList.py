6be96e773336a94a91b3215ab520a1ea67d3d399
marco <marco@PC39.europrofili.locale>
2018-12-13 17:35:43 +0100

    Add PCname and login user

--- pyProgramList/src/pyProgramList.py
+++ pyProgramList/src/pyProgramList.py
@@ -7,9 +7,19 @@ Created on 11 dic 2018
 '''
 
 from winreg import *
+import os
+import shutil
 
 key =  OpenKey(HKEY_LOCAL_MACHINE,r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", 0, KEY_READ)
 subkeynum = QueryInfoKey(key)[0]
+
+user = os.getlogin()
+pc = os.getenv("COMPUTERNAME","")
+print(os.environ.keys())
+
+#fileOutPath = os.path.join(os.environ["TEMP"],"{} programmi.txt".format(pc))
+fileOutPath = os.path.join(os.getenv('HOMEDRIVE', 'C:'),os.getenv('HOMEPATH'),'Desktop',"{}_programmi.txt".format(pc))
+fileOut = open(fileOutPath,"w")
 
 for i in range(subkeynum):
     subkeyname = EnumKey(key, i)
@@ -17,12 +27,34 @@ for i in range(subkeynum):
     subkey = OpenKey(key, subkeyname, 0, KEY_READ)
     try:
         if QueryInfoKey(subkey)[1] != 0: #sono presenti valori
-            print(QueryValueEx(subkey, "DisplayName")[0], end=';')
-            print(QueryValueEx(subkey, "DisplayVersion")[0])
+            name = QueryValueEx(subkey, "DisplayName")[0]
+            print(name)
+            version = QueryValueEx(subkey, "DisplayVersion")[0]
         else:
-            print()
+            name = ''
+            version = ''
     except:
-        print("NO Name")
+        name = ''
+        version = ''
+        
+    fileOut.write(";".join([pc,user,subkeyname,name,version]))
+    fileOut.write(os.linesep)
+
+fileOut.close()
+
+src = fileOutPath
+dst = os.path.join("savefiles","{}_programmi.txt".format(pc))
+shutil.copyfile(src, dst)
+
+print("\n\n\n")
+print("File {}_programmi.txt salvato sul DESKTOP.".format(pc))
+print("Leggetelo e inviatelo a Marco via mail segnalando")
+print("i programmi che già sapete essere senza licenza.")
+print("Vi invito a disinstallare ciò che non è in regola e che non vi serve.")
+print("A fine censimento valuteremo con la Direzione l'acquisto delle")
+print("licenze necessarie.\n\n")
+
+res = input("Premere INVIO per uscire. Grazie")
 
 if __name__ == '__main__':
     pass
